f8d168cdb3ede46adf6ba36b943adf8834468df8
Amol <amol.chawathe@fountaintechies.com>
2015-03-09 00:46:35 +0800

    # Added the functions folder # Added the index.php path
    Todo :
    # Add the ORM Controller to create the company

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+include_once "../functions/company.php" ;
+
+use Respect\Rest\Router;
+
+$r3 = new Router('/80startups/api/index.php');
+
+$r3->get('/hello', function() {
+    return 'Hello from Path';
+});
+
+
+
+$r3->get( '/addcompany' ,  'Add Company' );
+
+$r3->get('/add/*', function($companyname) {
+      /** do something */
+      addcompany($companyname);
+});
+
+
+
+
+?>
---
+++ functions/company.php
@@ -0,0 +1,7 @@
+<?php
+
+function addcompany($companyname){
+  echo $companyname ;
+}
+
+?>
