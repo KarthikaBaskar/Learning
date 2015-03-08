2cf7485b68bfa60d809d5cd1f7f17d7a9d63a9b7
Amol <amol.chawathe@fountaintechies.com>
2015-03-09 01:23:33 +0800

    Added the database component.

--- api/index.php
+++ api/index.php
@@ -4,23 +4,23 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 include_once "../functions/company.php" ;
 
-use Respect\Rest\Router;
 
+use Respect\Rest\Router;
 $r3 = new Router('/80startups/api/index.php');
 
 $r3->get('/hello', function() {
-    return 'Hello from Path';
-});
-
-
+        return 'Hello from Path';}
+);
 
 $r3->get( '/addcompany' ,  'Add Company' );
-
 $r3->get('/add/*', function($companyname) {
       /** do something */
       addcompany($companyname);
 });
 
+$r3->get('/list', function() {
+      listcompany();
+});
 
 
 
--- functions/company.php
+++ functions/company.php
@@ -1,7 +1,30 @@
 <?php
 
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Respect\Relational\Mapper;
+
+
+
 function addcompany($companyname){
+
   echo $companyname ;
 }
+
+
+
+function listcompany(){
+
+  $mapper = new Mapper(new PDO('mysql:host=localhost;port=3306;dbname=80startups','80startups','10gXWOqeaf'));
+  $companies = $mapper->company->fetchAll();
+  foreach ($companies as $company) {
+      echo $company->companyname . PHP_EOL;
+  }
+
+}
+
+
+
 
 ?>
